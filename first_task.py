82806ee5f163eb9795a68cc0a293824beaacca57
Nurlis <kimbiletovnurlis03@gmail.com>
2022-09-16 08:48:46 +0600

    Add first_task.py file

---
+++ first_task.py
@@ -0,0 +1,5 @@
+"""
+1.	Написать программу поиска самого длинного слова в строке, разделенной пробелами.
+Ales Blitz Burger Community Cut out Decadent Generosity Sapsan Semi-dark Tamer
+"""
+#print(max([len(i) for i in input("Строка: ").split()]))
